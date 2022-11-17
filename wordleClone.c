7f143b618891fa718109b6d0b0fbce31faef80c8
LowLevelLoser <108631435+LowLevelLoser@users.noreply.github.com>
2022-11-16 20:58:40 -0600

    Update wordleClone.c
    its now the same as the video

--- wordleClone.c
+++ wordleClone.c
@@ -34,8 +34,7 @@ int main(){
 		return 2;
 	}
 
-	int readLine = rand() % count;
-	readLine++;
+	int readLine = rand() % count + 1;
 
 	bool keep_reading = true;
 	int currentLine = 1;
@@ -61,10 +60,9 @@ int main(){
 
 	//start of game
 	printf("word size %ld\n", strlen(str1 + 1));
-	for(int x = 0; x < 6; x++){
-		bool loser = true;
+	for(int x = 0; x < 10; x++){
 		printf("\ngive me your guess\n");
-		fgets(str2, 20, stdin);
+		fgets(str2, 50, stdin);
 		if(mainLoop(str2, str1)){
 			printf("%s\nYou Win\n%s", TC_GRN, TC_NRM);
 			fclose(flptr);
@@ -85,6 +83,7 @@ bool mainLoop(char *wrt, char *str){
 	int chkS[50];
 	int chkW[50];
 	int c = 0;
+	bool winner = false;
 
 	if(strlen(wrt) != strlen(str)){
 		printf("%swrong size%s\n", TC_RED, TC_NRM);
@@ -105,26 +104,28 @@ bool mainLoop(char *wrt, char *str){
 			c++;
 		}
 	}
-
-	for(int w = 0; w < strlen(wrt); w++){
-		for(int s = 0; s < strlen(str); s++){
-			if((chkS[s] == 0) && (chkW[w] == 0)) {
-				if(wrt[w] == str[s]){
-					com[w] = wrt[w]*2;
-					chkS[s] = 1;
-					chkW[w] = 1;
+	if(c == strlen(str)){
+		winner = true;
+	}
+	else{
+		for(int w = 0; w < strlen(wrt); w++){
+			for(int s = 0; s < strlen(str); s++){
+				if((chkS[s] == 0) && (chkW[w] == 0)) {
+					if(wrt[w] == str[s]){
+						com[w] = wrt[w]*2;
+						chkS[s] = 1;
+						chkW[w] = 1;
+					}
+					else{
+						com[w] = -1*wrt[w];
+					}
 				}
-				else{
-					com[w] = -2*wrt[w];
-				}
-
 			}
-		}
-	}	
-
+		}	
+	}
 	for(int i = 0; i < strlen(str + 1); i++){
 		if(com[i] <= 0){
-			printf("%s%c%s",TC_RED, -1*com[i]/2, TC_NRM);
+			printf("%s%c%s",TC_RED, -1*com[i], TC_NRM);
 		}
 		else if(com[i] % 2 == 1){
 			printf("%s%c%s",TC_GRN, com[i]/2, TC_NRM);
@@ -134,6 +135,5 @@ bool mainLoop(char *wrt, char *str){
 		}
 	}
 
-	return c == strlen(str);
+	return winner;
 }
-
